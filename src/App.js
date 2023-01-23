b04f4ad196cfb2ff72539ca6bdf3aa5dc4c0fed7
Alexander Diniz <automaalex@hotmail.com>
2023-01-23 16:42:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./App.css";
+
+// import required modules
+import { EffectCoverflow, Navigation, Pagination } from "swiper";
+import Footer from "./components/Footer";
 
 function App() {
+  const [referral, setReferral] = useState("");
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = () => {
+      fetch("/tools/tools.json")
+        .then((r) => r.json())
+        .then(setProjects)
+        .catch(console.error);
+    };
+
+    const query = new URLSearchParams(window.location.search);
+    const r = query.get("r");
+    console.log(r);
+    if (r) {
+      setReferral(r);
+    }
+
+    fetchProjects();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h1>Hello, </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {referral && `It seems you like ${referral}!`} Here are some{" "}
+          {referral && "other"} tools i made for utility and fun.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+      <div className="slides">
+        <Swiper
+          effect={"coverflow"}
+          slidesPerView="1"
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            slideShadows: true,
+          }}
+          pagination={true}
+          navigation={true}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="mySwiper"
         >
-          Learn React
-        </a>
-      </header>
+          {projects.length > 0 &&
+            projects.map((e) => (
+              <SwiperSlide>
+                <a href={e.url} target="__blank">
+                  <div className="project-info">
+                    <h2>{e.name}</h2>
+                  </div>
+                  <img
+                    alt="project information"
+                    src={"/tools/img/" + e.img + ".jpg"}
+                  />
+                </a>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
+      <Footer />
     </div>
   );
 }
